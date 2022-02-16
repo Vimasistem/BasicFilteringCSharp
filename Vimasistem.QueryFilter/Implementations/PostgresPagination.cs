13a369f47aa3d33722b212df29aeaf6f688273a9
Jose Villalta <josemiguelvillaltah@gmail.com>
2022-02-16 10:32:53 -0500

    postgresql pagination implementation

---
+++ Vimasistem.QueryFilter/Implementations/PostgresPagination.cs
@@ -0,0 +1,22 @@
+using Vimasistem.QueryFilter.Interfaces;
+
+namespace Vimasistem.QueryFilter.Implementations
+{
+    public class PostgresPagination : IPagination
+    {
+        public string GetQuery(int? pageNumber, int? pageSize)
+        {
+            string query = "";
+
+            if (pageSize != null) query = " " + query + $"LIMIT {pageSize}";
+            
+            if (pageNumber != null)
+            {
+                pageNumber = (pageNumber - 1) * (pageSize ?? 1);
+                query =  " " + query + $"OFFSET {pageNumber} ROWS";
+            }
+
+            return query;
+        }
+    }
+}
