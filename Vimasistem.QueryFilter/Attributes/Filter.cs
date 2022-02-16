f13a150697ba1a60010660835b3be72cae809e9b
Jose Villalta <josemiguelvillaltah@gmail.com>
2022-02-16 10:32:18 -0500

    Filter attribute

---
+++ Vimasistem.QueryFilter/Attributes/Filter.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Vimasistem.QueryFilter.Attributes
+{
+    [AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+    sealed class Filter : Attribute
+    {
+        private readonly string _fieldName;
+        private readonly string _group;
+        
+        public Filter(string field)
+        {
+            _fieldName = field;
+        }
+
+        public Filter(string field, string group)
+        {
+            _fieldName = field;
+            _group = group;
+        }
+
+        public string FieldName => _fieldName;
+
+        public string Group => _group;
+    }
+}
