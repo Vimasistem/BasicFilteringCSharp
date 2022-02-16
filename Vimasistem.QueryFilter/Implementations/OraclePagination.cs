511b0c07b7f9467665ee28ea482fe400fcd56f4f
Jose Villalta <josemiguelvillaltah@gmail.com>
2022-02-16 10:33:01 -0500

    oracle pagination implementation

---
+++ Vimasistem.QueryFilter/Implementations/OraclePagination.cs
@@ -0,0 +1,22 @@
+using Vimasistem.QueryFilter.Interfaces;
+
+namespace Vimasistem.QueryFilter.Implementations
+{
+    public class OraclePagination : IPagination
+    {
+        public string GetQuery(int? pageNumber, int? pageSize)
+        {
+            string query = "";
+
+            if (pageNumber != null)
+            {
+                pageNumber = (pageNumber - 1) * (pageSize ?? 1);
+                query =  " " + query + $"OFFSET {pageNumber} ROWS";
+            }
+            
+            if (pageSize != null) query = " " + query + $"FETCH NEXT {pageSize} ROWS ONLY";
+
+            return query;
+        }
+    }
+}
